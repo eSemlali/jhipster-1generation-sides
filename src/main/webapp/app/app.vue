4adc6d76f6e1c851879b132a46d32c23a415a5b5
semlali <e.semlali.mohamed@gmail.com>
2022-03-23 09:08:24 +0100

    Initial version of jhipster generated by generator-jhipster@7.7.0

---
+++ src/main/webapp/app/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <ribbon></ribbon>
+    <div id="app-header">
+      <jhi-navbar></jhi-navbar>
+    </div>
+    <div id="app-main">
+      <div id="app-side">
+        <jhi-sidebar></jhi-sidebar>
+      </div>
+      <div class="container-fluid">
+        <div class="card jh-card">
+          <router-view></router-view>
+        </div>
+        <b-modal id="login-page" hide-footer lazy>
+          <span data-cy="loginTitle" slot="modal-title" id="login-title" v-text="$t('login.title')">Sign in</span>
+          <login-form></login-form>
+        </b-modal>
+      </div>
+    </div>
+    <div id="app-footer">
+      <jhi-footer></jhi-footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./app.component.ts"></script>
+
+<style scoped>
+#app-main {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+  margin: 0 !important;
+}
+</style>
---
+++ src/main/webapp/app/core/jhi-sidebar/jhi-sidebar.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="sidebar">
+    <div class="menu-items">
+      <b-navbar-nav>
+        <b-nav-item class="items" to="/" exact>
+          <span>
+            <font-awesome-icon icon="home" />
+            <span>Home</span>
+          </span>
+        </b-nav-item>
+
+        <b-nav-item-dropdown right id="entity-menu" v-if="authenticated" active-class="active" class="pointer items" data-cy="entity">
+          <span slot="button-content" class="navbar-dropdown-menu">
+            <font-awesome-icon icon="th-list" />
+            <span class="no-bold" v-text="$t('global.menu.entities.main')">Entities</span>
+          </span>
+          <entities-menu></entities-menu>
+          <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown
+          id="admin-menu"
+          v-if="hasAnyAuthority('ROLE_ADMIN') && authenticated"
+          :class="{ 'router-link-active': subIsActive('/admin') }"
+          active-class="active"
+          class="pointer items"
+          data-cy="adminMenu"
+        >
+          <span slot="button-content" class="navbar-dropdown-menu">
+            <font-awesome-icon icon="users-cog" />
+            <span class="no-bold" v-text="$t('global.menu.admin.main')">Administration</span>
+          </span>
+          <b-dropdown-item to="/admin/user-management" active-class="active">
+            <font-awesome-icon icon="users" />
+            <span v-text="$t('global.menu.admin.userManagement')">User management</span>
+          </b-dropdown-item>
+          <b-dropdown-item to="/admin/metrics" active-class="active">
+            <font-awesome-icon icon="tachometer-alt" />
+            <span v-text="$t('global.menu.admin.metrics')">Metrics</span>
+          </b-dropdown-item>
+          <b-dropdown-item to="/admin/health" active-class="active">
+            <font-awesome-icon icon="heart" />
+            <span v-text="$t('global.menu.admin.health')">Health</span>
+          </b-dropdown-item>
+          <b-dropdown-item to="/admin/configuration" active-class="active">
+            <font-awesome-icon icon="cogs" />
+            <span v-text="$t('global.menu.admin.configuration')">Configuration</span>
+          </b-dropdown-item>
+          <b-dropdown-item to="/admin/logs" active-class="active">
+            <font-awesome-icon icon="tasks" />
+            <span v-text="$t('global.menu.admin.logs')">Logs</span>
+          </b-dropdown-item>
+          <b-dropdown-item v-if="openAPIEnabled" to="/admin/docs" active-class="active">
+            <font-awesome-icon icon="book" />
+            <span v-text="$t('global.menu.admin.apidocs')">API</span>
+          </b-dropdown-item>
+          <b-dropdown-item v-if="!inProduction" href="./h2-console/" target="_tab">
+            <font-awesome-icon icon="database" />
+            <span v-text="$t('global.menu.admin.database')">Database</span>
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown class="items" id="languagesnavBarDropdown" right v-if="languages && Object.keys(languages).length > 1">
+          <span slot="button-content">
+            <font-awesome-icon icon="flag" />
+            <span class="no-bold" v-text="$t('global.menu.language')">Language</span>
+          </span>
+          <b-dropdown-item
+            v-for="(value, key) in languages"
+            :key="`lang-${key}`"
+            v-on:click="changeLanguage(key)"
+            :class="{ active: isActiveLanguage(key) }"
+          >
+            {{ value.name }}
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown
+          href="javascript:void(0);"
+          id="account-menu"
+          :class="{ 'router-link-active': subIsActive('/account') }"
+          active-class="active"
+          class="pointer items"
+          data-cy="accountMenu"
+        >
+          <span slot="button-content" class="navbar-dropdown-menu">
+            <font-awesome-icon icon="user" />
+            <span class="no-bold" v-text="$t('global.menu.account.main')"> Account </span>
+          </span>
+          <b-dropdown-item data-cy="settings" to="/account/settings" tag="b-dropdown-item" v-if="authenticated" active-class="active">
+            <font-awesome-icon icon="wrench" />
+            <span v-text="$t('global.menu.account.settings')">Settings</span>
+          </b-dropdown-item>
+          <b-dropdown-item data-cy="passwordItem" to="/account/password" tag="b-dropdown-item" v-if="authenticated" active-class="active">
+            <font-awesome-icon icon="lock" />
+            <span v-text="$t('global.menu.account.password')">Password</span>
+          </b-dropdown-item>
+          <b-dropdown-item data-cy="logout" v-if="authenticated" v-on:click="logout()" id="logout" active-class="active">
+            <font-awesome-icon icon="sign-out-alt" />
+            <span v-text="$t('global.menu.account.logout')">Sign out</span>
+          </b-dropdown-item>
+          <b-dropdown-item data-cy="login" v-if="!authenticated" v-on:click="openLogin()" id="login" active-class="active">
+            <font-awesome-icon icon="sign-in-alt" />
+            <span v-text="$t('global.menu.account.login')">Sign in</span>
+          </b-dropdown-item>
+          <b-dropdown-item
+            data-cy="register"
+            to="/register"
+            tag="b-dropdown-item"
+            id="register"
+            v-if="!authenticated"
+            active-class="active"
+          >
+            <font-awesome-icon icon="user-plus" />
+            <span v-text="$t('global.menu.account.register')">Register</span>
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./jhi-sidebar.component.ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* ==========================================================================
+    Navbar
+    ========================================================================== */
+.sidebar {
+  position: relative;
+  background-color: #fafafa;
+  height: 100%;
+}
+.items {
+  background-color: #33445511;
+  padding: 2px;
+  padding-left: 15px;
+  margin-top: 5px;
+  color: #333;
+}
+</style>
